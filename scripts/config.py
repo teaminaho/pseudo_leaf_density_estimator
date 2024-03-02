4cc889a3d50449ab41d3494042d122939ecfe0fc
yuki-inaho <yoshikawa@inaho.co>
2024-03-02 18:58:06 +0900

    Refactor from_bottom_to_top.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,6 +1,6 @@
 import toml
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 
 @dataclass
@@ -9,18 +9,18 @@ class LDIConfig:
     lsh_upper: List[float]
     hsv_lower: List[int]
     hsv_upper: List[int]
-    grid_size: int
-    kernel_size: int
-    density_min: int
-    num_density_bins: int
-    divided_area: List[int]
-    k_h_size: int
-    serch_area: List[int]
-    crop_rate: List[float] = None
-    border_color: List[List[int]] = None
+    grid_size: Optional[int] = None
+    kernel_size: Optional[int] = None
+    density_min: Optional[int] = None
+    num_density_bins: Optional[int] = None
+    divided_area: Optional[List[int]] = None
+    k_h_size: Optional[int] = None
+    serch_area: Optional[List[int]] = None
+    horizontal_crop_ratio_list: Optional[List[float]] = None
+    border_color: Optional[List[List[int]]] = None
 
     @staticmethod
-    def from_toml(config_path: str):
+    def from_toml(config_path: str) -> "LDIConfig":
         with open(config_path, "r") as f:
             data = toml.load(f)
         return LDIConfig(**data)
